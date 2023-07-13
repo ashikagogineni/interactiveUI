58275dd0821917e024768ae10f78a88acd6bb804
Ashika G <ashi.gogineni@gmail.com>
2023-07-13 11:21:01 -0700

    Initial Commit

---
+++ interactiveUI/interactiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveUIApp.swift
+//  interactiveUI
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct interactiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
