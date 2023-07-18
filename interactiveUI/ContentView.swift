cfda07207d5c02d35fbe1a90004a89c496d07054
Ashika G <ashi.gogineni@gmail.com>
2023-07-18 10:30:27 -0700

    test

--- interactiveUI/ContentView.swift
+++ interactiveUI/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What's your name?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Text(textTitle)
+                .font(.title)
+            TextField("Type name here", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title2)
+                .border(Color.gray, width: 1)
+            Button("Submit Name"){
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title3)
+            .buttonStyle(.borderedProminent)
+            .tint(.blue)
         }
         .padding()
+    }
+}
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var dropDown: UIPickerView!
+
+    var list = ["1", "2", "3"]
+
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+
+        return list.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        self.view.endEditing(true)
+        return list[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        self.textBox.text = self.list[row]
+        self.dropDown.isHidden = true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+
+        if textField == self.textBox {
+            self.dropDown.isHidden = false
+            //if you don't want the users to se the keyboard type:
+
+            textField.endEditing(true)
+        }
     }
 }
 
